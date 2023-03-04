d8f88d7c13c37929659ed4d840dc92691bda8e4b
Alex MKX <github-alex@xxl.cx>
2023-03-04 11:27:53 +0400

    initial

---
+++ tf-module/main.tf
@@ -0,0 +1,25 @@
+variable "project_id" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "query" {
+  type = string
+}
+variable "service_account_json_contents" {
+  type = string
+  sensitive = true
+}
+data "external" "docker_bq_proxy" {
+  program = ["docker", "run", "--rm", "-i", "gfncis/tf-bq-proxy"]
+  query   = {
+    service_account = var.service_account_json_contents
+    project_id      = var.project_id
+    location        = var.location
+    query           = var.query
+  }
+}
+output "result" {
+  value = jsondecode(jsondecode(data.external.docker_bq_proxy.result.result))
+}
