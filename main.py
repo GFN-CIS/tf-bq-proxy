38024b148234779fae6541e1482597a86d0b7637
Alex MKX <github-alex@xxl.cx>
2023-03-10 19:00:06 +0400

    added scopes

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ data = json.load(sys.stdin)
 fd, sa_file = tempfile.mkstemp()
 with open(sa_file, 'w') as f:
     f.write(data['service_account'])
-credentials = service_account.Credentials.from_service_account_file(sa_file)
+credentials = service_account.Credentials.from_service_account_file(sa_file, scopes=[
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/cloud-platform",
+])
 client = bigquery.Client(credentials=credentials, project=data['project_id'], location=data['location'])
 query_job = client.query(
     data['query']
