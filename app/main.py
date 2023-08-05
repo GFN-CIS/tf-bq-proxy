00a9dc6658bd6f8477ec03e9002dcbe081e303a7
Alex MKX <github-alex@xxl.cx>
2023-08-05 11:25:25 +0400

    added attribution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import os, sys, json
 from google.cloud import bigquery
-from google.oauth2 import service_account
-import tempfile
+from google.oauth2 import credentials
 # import pydevd_pycharm
 #
 # pydevd_pycharm.settrace('localhost', port=6456, stdoutToServer=True, stderrToServer=True)
@@ -9,7 +8,7 @@ try:
     data = json.load(sys.stdin)
 except:
     data = json.loads(os.environ['TF_BQ_PROXY_DATA'])
-credentials = service_account.Credentials.from_service_account_file(data['access_token'])
+credentials = credentials.Credentials(token=data['access_token'])
 
 client = bigquery.Client(credentials=credentials, project=data['project_id'], location=data['location'])
 query_job = client.query(
